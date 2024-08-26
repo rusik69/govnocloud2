703cdc7c8cd782abd9c3544d38ca4b61d5811b27
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2024-08-27 01:10:53 +0200

    add change node id to nodes

--- pkg/k3s/node.go
+++ pkg/k3s/node.go
@@ -1,15 +1,12 @@
 package k3s
 
 import (
-	"log"
-
 	"github.com/rusik69/govnocloud2/pkg/ssh"
 )
 
 // DeployNode deploys k3s nodes.
 func DeployNode(host, user, key, master, token string) error {
-	cmd := "curl -sfL https://get.k3s.io | K3S_URL=https://" + master + ":6443 K3S_TOKEN=" + token + " sh -"
-	log.Println(cmd)
+	cmd := "curl -sfL https://get.k3s.io | K3S_URL=https://" + master + ":6443 K3S_TOKEN=" + token + " INSTALL_K3S_EXEC='--node-name=" + host + "' sh -"
 	_, err := ssh.Run(cmd, host, key, user, true)
 	if err != nil {
 		return err
