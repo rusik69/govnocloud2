795b011274a97d8569c7d2084e199000bd0bb2e4
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2024-08-31 18:06:09 +0200

    fix helm install url

--- pkg/k3s/helm.go
+++ pkg/k3s/helm.go
@@ -8,7 +8,8 @@ import (
 
 // InstallHelm installs Helm to k3s cluster.
 func InstallHelm() error {
-	command := exec.Command("curl", "-sfL", "https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash")
+	cmd := "curl -sfL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash"
+	command := exec.Command("bash", "-c", cmd)
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	if err := command.Run(); err != nil {
