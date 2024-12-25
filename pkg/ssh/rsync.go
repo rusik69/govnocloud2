afc0dcd27bed6adf4f68090f92d987c2268346d6
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2024-12-25 01:39:07 +0100

    fix vm image

--- pkg/ssh/rsync.go
+++ pkg/ssh/rsync.go
@@ -8,7 +8,7 @@ import (
 
 // Rsync copies files from source to destination using rsync
 func Rsync(src, dst, host, user, key string) error {
-	cmd := fmt.Sprintf("rsync -e 'ssh -i %s' -avz %s %s@%s:%s", key, src, user, host, dst)
+	cmd := fmt.Sprintf("rsync --rsync-path='mkdir -p /var/www/govnocloud2 && rsync' -e 'ssh -i %s' -avz %s %s@%s:%s", key, src, user, host, dst)
 	log.Println(cmd)
 	out, err := exec.Command("sh", "-c", cmd).CombinedOutput()
 	if err != nil {
