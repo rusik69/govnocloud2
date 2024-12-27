394a4f11c01727f5921f8394c2500519e376cb07
Ruslan Gustomiasov <rgustomiasov@mirantis.com>
2024-12-27 20:18:09 +0100

    vms fix

--- pkg/k3s/kubevirt.go
+++ pkg/k3s/kubevirt.go
@@ -63,7 +63,7 @@ func InstallKubeVirt(host, user, key string) error {
 		return fmt.Errorf("failed to wait for KubeVirt to be ready: %w", err)
 	}
 	// FIXME: wait for crds
-	time.Sleep(1 * time.Second)
+	time.Sleep(5 * time.Second)
 	// Create kubevirt instance types
 	for _, size := range types.VMSizes {
 		cmd := fmt.Sprintf("virtctl create instancetype --name %s --cpu %d --memory %dMi | kubectl create -f -", size.Name, size.CPU, size.RAM)
